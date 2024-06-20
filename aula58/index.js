b37f337b69008baec1a256a9c32c1dfc929f8f06
akiwnl <carvalhokaike69@gmail.com>
2024-06-19 22:16:00 -0300

    constructor functions

--- aula58/index.js
+++ aula58/index.js
@@ -10,3 +10,16 @@ function Person(name, lastName) {
 const p1 = new Person('Kaike', 'Carvalho');
 
 console.log(p1.name)
+
+/*
+Há também diferenças na herança e prototipagem. Enquanto a Constructor Function utiliza a cadeia de prototipos
+que permite a definição de métodos no protótipo do construtor, a Factory Function não utiliza desse meio e isso
+pode trazer alguns problemas como a duplicação de métodos e maior consumo de memória.
+
+O uso do `this` na Factory Function depende do contexto em que a função é chamada, enquanto na Constructor Function,
+o `this` é o novo objeto criado pelo `new`.
+
+Para encerrar, ambos os padroes tem suas vantagens e desvantagens. As Factory Functions são mais simples e seguras 
+em muitos casos, enquanto as Constructor Functions oferecem suporte nativo à herança baseada em protótipos do JavaScript.
+A escolha entre um e outro depende do contexto e das necessidades específicas da aplicação.
+*/
